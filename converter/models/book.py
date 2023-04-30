ddde7eda25e87ac4df6e49787a041a9b0c33ffc9
Eloa Mello <eloamello126@gmail.com>
2023-04-30 15:03:36 -0300

    feat: added audiobook attribute and added cascade mode on delete

--- converter/models/book.py
+++ converter/models/book.py
@@ -1,12 +1,7 @@
 from django.db import models
 from .genre import Genre
-
-import boto3
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class S3MediaStorage(S3Boto3Storage):
-    location = 'books'
+from .s3mediastorage import S3MediaStorage
+from .audiobook import AudioBook
 
 
 class Book(models.Model):
@@ -14,8 +9,11 @@ class Book(models.Model):
     book_description = models.TextField(db_column='description', blank=False)
     book_author = models.CharField(db_column='author', max_length=200, blank=False)
     book_year = models.IntegerField(db_column='year', blank=False)
-    book_genres = models.ManyToManyField(Genre, related_name='genre_books', blank=False)
-    book_file = models.FileField(upload_to='books/', storage=S3MediaStorage(), null=False, blank=False)
+    audio_book = models.OneToOneField(AudioBook, related_name='audio_book',
+                                      on_delete=models.CASCADE, null=False, blank=False)
+    book_genres = models.ManyToManyField(Genre, related_name='book_genres', blank=False)
+    book_file = models.FileField(upload_to='books/', storage=S3MediaStorage(),
+                                 on_delete=models.CASCADE, null=False, blank=False)
 
     def __str__(self):
         return self.book_title
