aff3afe1191e49484c595e2095011482805595db
Eloa Mello <eloamello126@gmail.com>
2023-05-02 11:37:50 -0300

    refactor: allowed null attribute of audio_book

--- converter/models/book.py
+++ converter/models/book.py
@@ -9,7 +9,8 @@ class Book(models.Model):
     book_description = models.TextField(db_column='description', blank=False)
     book_author = models.CharField(db_column='author', max_length=200, blank=False)
     book_year = models.IntegerField(db_column='year', blank=False)
-    audio_book = models.OneToOneField(AudioBook, related_name='audio_book', on_delete=models.CASCADE)
+    audio_book = models.OneToOneField(AudioBook, related_name='audio_book',
+                                      on_delete=models.CASCADE, null=True)
     book_genres = models.ManyToManyField(Genre, related_name='book_genres', blank=False)
     book_file = models.FileField(upload_to='books/', storage=S3MediaStorage(), null=False, blank=False)
 
