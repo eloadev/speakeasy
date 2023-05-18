9c627880c10892d5a396b4eed1415e8599dca2e1
Eloa Mello <eloamello126@gmail.com>
2023-05-18 17:40:54 -0300

    feat: account urls and commented book and genre urls (temporarily)

--- speakeasy/urls.py
+++ speakeasy/urls.py
@@ -2,13 +2,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from converter.views import book, genre, index
+from converter.views.user import create_user, CustomSignupView, CustomLoginView
 
 urlpatterns = [
     path('', index.index, name="index"),
-    path('book/create', book.book_create, name="book_create"),
-    path('book/delete/<int:book_id>', book.book_delete, name='book_delete'),
-    path('book/retrieve/<int:book_id>', book.book_retrieve, name='book_retrieve'),
-    path('genre/create', genre.genre_create, name="genre_create"),
+    #path('book/create', book.book_create, name="book_create"),
+    #path('book/delete/<int:book_id>', book.book_delete, name='book_delete'),
+    #path('book/retrieve/<int:book_id>', book.book_retrieve, name='book_retrieve'),
+    #path('genre/create', genre.genre_create, name="genre_create"),
+    path('account/', include('allauth.urls')),
+    path('create-user/', create_user, name='create_user'),
+    path('account/signup/', CustomSignupView.as_view(), name='account_signup'),
+    path('account/login/', CustomLoginView.as_view(), name='account_login'),
 ]
 
 
